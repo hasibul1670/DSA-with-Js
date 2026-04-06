f5af6e69cc4a63ba77782d3ff45af0c86954bdca
Hasibul Islam <hasibulislam1670@gmail.com>
2026-04-06 23:52:22 +0600

    Fact :: Add  insertion sort Psudocode !

--- insertion.js
+++ insertion.js
@@ -9,6 +9,31 @@
 // Pass 1: [5, 12, 8, 3, 15, 7]
 // Pass 2: [5, 8, 12, 3, 15, 7]
 
+
+// start from index 1
+
+// for each element in array:
+//     take current element
+
+//     compare it with left side elements
+
+//     while left element is bigger:
+//         shift left element to right
+
+//     place current element in correct position
+
+
+// FOR i = 1 to n-1:
+//     current = arr[i]
+//     j = i - 1
+
+//     WHILE j >= 0 AND arr[j] > current:
+//         arr[j + 1] = arr[j]
+//         j = j - 1
+
+//     arr[j + 1] = current
+
+
 const arr = [12, 5, 8, 3, 15, 7];
 
 console.log("Start:", arr.join(" "));
